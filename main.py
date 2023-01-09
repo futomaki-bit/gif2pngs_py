9e7cdd9fc86ef63b44427f6558060a39e7c85583
futomakiyoin <72528988+futomaki-bit@users.noreply.github.com>
2023-01-09 14:34:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from PIL import Image
+import os
 
-# !!! Put the gif's filename here
-gifname = 'test.gif'
+giffolder = "./gifs"  # gif source folder
+outfolder = "./output"  # gif frames output folder
 
-with Image.open(gifname) as im:
-    # for every frame in gif
-    for i in range(im.n_frames):
-        # get frame
-        im.seek(i)
-        # save frame
-        im.save(f'{i}.png')
+# create a folder
+def createfolder(foldername):
+    if not os.path.exists(f"./{foldername}"):
+        os.makedirs(f"./{foldername}")
+
+
+# create output folder
+createfolder(outfolder)
+# for all files in giffolder
+for fi in os.listdir(giffolder):
+    # if file is of .gif extension
+    if fi.split('.')[-1] == 'gif':
+        # open image as Pillow(from PIL) Image
+        with Image.open(giffolder + "/" + fi) as im:
+            print(fi)  # just to update on what the program is working on
+
+            # create destination folder
+            createfolder(f'{outfolder}/{fi}')
+
+            # for every frame in gif
+            for i in range(im.n_frames):
+                # get frame
+                im.seek(i)
+                # save frame
+                im.save(f'{outfolder}/{fi}/{i}.png')
